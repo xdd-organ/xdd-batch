9caf19d77667e1bdb807946f3aaf8946d64fe56b
xdd <767403510@qq.com>
2019-03-13 18:02:27 +0800

    新增配置

--- src/main/java/com/java/xdd/batch/configuration/BatchConfig.java
+++ src/main/java/com/java/xdd/batch/configuration/BatchConfig.java
@@ -8,18 +8,23 @@ import com.java.xdd.batch.processor.HelloItemProcessor;
 import com.java.xdd.batch.processor.Processor;
 import com.java.xdd.batch.reader.Reader;
 import com.java.xdd.batch.writer.Writer;
+import org.springframework.batch.core.ItemReadListener;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.file.FlatFileFooterCallback;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.LineCallbackHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+
+import java.io.IOException;
 
 @Configuration
 public class BatchConfig {
@@ -42,7 +47,23 @@ public class BatchConfig {
 
     @Bean
     public Step orderStep1() {
-        return stepBuilderFactory.get("orderStep1").<DeviceCommand, DeviceCommand> chunk(1)
+        return stepBuilderFactory.get("orderStep1").<DeviceCommand, DeviceCommand> chunk(1).listener(new ItemReadListener() {
+            @Override
+            public void beforeRead() {
+//                throw new RuntimeException();
+//                System.out.println("fsa");
+            }
+
+            @Override
+            public void afterRead(Object o) {
+//                System.out.println("fsa1");
+            }
+
+            @Override
+            public void onReadError(Exception e) {
+
+            }
+        })
                 .reader(this.getRead()).processor(processor)
                 .writer(this.getWrite()).build();
     }
@@ -52,6 +73,12 @@ public class BatchConfig {
         FlatFileItemWriter<DeviceCommand> write = new FlatFileItemWriter<>();
         write.setResource(new FileSystemResource("E:\\yidong\\xdd-batch\\src\\main\\resources\\batch-data1.csv"));
         write.setLineAggregator(new HelloLineAggregator());
+        write.setFooterCallback(new FlatFileFooterCallback() {
+            @Override
+            public void writeFooter(java.io.Writer writer) throws IOException {
+                writer.append("111");//再尾部追加
+            }
+        });
         return write;
     }
 
@@ -60,6 +87,13 @@ public class BatchConfig {
         FlatFileItemReader<DeviceCommand> reader = new FlatFileItemReader<>();
         reader.setResource(new FileSystemResource("E:\\yidong\\xdd-batch\\src\\main\\resources\\batch-data.csv"));
         reader.setLineMapper(new HelloLineMapper());
+        reader.setSkippedLinesCallback(new LineCallbackHandler(){
+            @Override
+            public void handleLine(String s) {
+                System.out.println(s);//跳过行的内容
+            }
+        });
+        reader.setLinesToSkip(5);//前多少行需要跳过
         return reader;
     }
 
--- src/main/java/com/java/xdd/batch/controller/JobInvokerController.java
+++ src/main/java/com/java/xdd/batch/controller/JobInvokerController.java
@@ -1,6 +1,7 @@
 package com.java.xdd.batch.controller;
 
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
